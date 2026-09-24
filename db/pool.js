8d5532dfcb2a2cabd4746666cc53aa244157ed74
Sparsh <sprshmhjn@gmail.com>
2026-09-24 15:28:29 +0530

    Configure SSL for Neon database

--- db/pool.js
+++ db/pool.js
@@ -5,5 +5,8 @@ module.exports = new Pool({
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
